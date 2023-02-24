643a30672b5703da517632c5d4c354a6e096daf1
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-02-24 15:55:49 +0000

    node developed

---
+++ READ_ME.md
@@ -0,0 +1,5 @@
+# wet
+  ![Github license] (https://img.shields.io/badge/license-MIT-blue.svg)
+
+
+
--- index.js
+++ index.js
@@ -4,50 +4,59 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = () => inquirer.prompt([
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?',
-    },
-    {
-        type: 'input',
-        name: 'location',
-        message: 'Where are you from?',
-    },
-    {
-        type: 'input',
-        name: 'hobby',
-        message: 'What is your favorite hobby?',
-    },
-    {
-        type: 'input',
-        name: 'food',
-        message: 'What is your favorite food?',
-    },
+const questions = [
     {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username',
+        message: 'What is your Github username?',
     },
     {
         type: 'input',
-        name: 'linkedin',
-        message: 'Enter your LinkedIn URL.',
+        name: 'email',
+        message: 'What is your email address?',
     },
-]);
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the project name?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe the project',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license?',
+        choices: ["MIT", "APACHE 2.0", "The Unlicense", "Boost Software License 1.0", "None"]
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What is the installation process?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Explain the test / contribution process',
+    },
+];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 
 }
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions).then((response) => {
+        console.log(`generating README...`);
+        writeToFile('READ_ME.md', generateMarkdown({ ...response }))
+    })
 
 }
 
 // function call to initialize program
 init();
-// questions();
-
